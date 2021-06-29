cf88939b4d3062b8af75149498a32374f5cce734
MatheusFrez <matheus.frez@psicologiaviva.com.br>
2021-06-29 19:26:03 -0300

    :rocket: Progress on home page.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
 
-import {
-  View
-} from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
 
 import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma na md10'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '2',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '2',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma na md10'
+    }
+  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -23,12 +51,32 @@ export function Home() {
         <Profile />
         <ButtonAdd />
       </View>
-      <View>
-        <CategorySelect 
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+
+      <CategorySelect 
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader 
+          title="Partidas agendadas" 
+          subtitle="Total 6"
+        />
+
+        <FlatList 
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Appointment data={item} />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
         />
       </View>
-    </View>
+    </View> 
   )
 }
+
+// Flat list e como um infinity scroll e é mais performática por isso 
+// do que a scroll view que renderiza todos elementos de uma lista
